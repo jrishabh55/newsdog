48dbc7c1f80261060dd621129b86fa60cf8c856e
Rishabh Jain <jrishabh55@gmail.com>
2017-08-15 15:49:41 +0530

    Server API Build Test

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -18,15 +18,28 @@ webpackEmptyAsyncContext.id = "../../../../../src lazy recursive";
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Contract; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_rxjs_add_operator_map__ = __webpack_require__("../../../../rxjs/add/operator/map.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_rxjs_add_operator_map___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_rxjs_add_operator_map__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_http__ = __webpack_require__("../../../http/@angular/http.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map__ = __webpack_require__("../../../../rxjs/add/operator/map.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map__);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
 
 var Contract = (function () {
     function Contract(http) {
         this.http = http;
-        this.host = 'localhost';
+        this.host = '';
         this.protocol = 'http';
-        this.port = 3000;
+        this.port = 80;
     }
     Contract.prototype.get = function (url, options) {
         return this.http.get(url, options).map(function (res) { return res.json(); });
@@ -35,12 +48,18 @@ var Contract = (function () {
         return this.http.post(url, body, options).map(function (res) { return res.json(); });
     };
     Contract.prototype.buildUrl = function (url) {
-        if (this.host.length === 0) {
-            return "/api/admin/" + url;
+        if (this.host.length <= 1) {
+            return "api/admin/" + url;
         }
+        console.log(this.host.length);
         return this.protocol + "://" + this.host + ":" + this.port + "/api/admin/" + url;
     };
+    Contract = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
+        __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_http__["Http"] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_http__["Http"]) === "function" && _a || Object])
+    ], Contract);
     return Contract;
+    var _a;
 }());
 
 //# sourceMappingURL=Contract.js.map
--- src/app/api/Contract.ts
+++ src/app/api/Contract.ts
@@ -1,12 +1,13 @@
+import {Injectable} from '@angular/core';
 import { Http, RequestOptionsArgs } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-
+@Injectable()
 export class Contract {
 
-  host: String = 'localhost';
+  host: String = '';
   protocol: String = 'http';
-  port = 3000;
+  port = 80;
 
   constructor(private http: Http) { }
 
@@ -19,9 +20,10 @@ export class Contract {
   }
 
   buildUrl(url: string): string {
-    if (this.host.length === 0) {
-      return `/api/admin/${url}`;
+    if (this.host.length <= 1) {
+      return `api/admin/${url}`;
     }
+    console.log(this.host.length);
     return `${this.protocol}://${this.host}:${this.port}/api/admin/${url}`;
   }
 }
