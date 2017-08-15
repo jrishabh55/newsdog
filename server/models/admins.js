0ccb06283c37b3c4710098986aff0dd85dbc290d
Rishabh Jain <jrishabh55@gmail.com>
2017-08-15 17:45:20 +0530

    Required validation fixed

--- server/models/admins.js
+++ server/models/admins.js
@@ -5,55 +5,54 @@ const connection = require("../connection");
 const bcrypt = require("bcrypt");
 
 const AdminSchema = new Schema({
-	name: {type: String, required: true, lowercase: true},
-	username: {type: String, required: true, unique: true, lowercase: true},
-	password: {type: String, required: true},
-	activated: {type: Boolean, default: false},
-	email: {type: String, required: true, unique: true, lowercase: true},
-	created_at: {type: Date, default: Date.now()},
-	access_level: {
-		admin: {type: Boolean, default: true},
-	},
+  name: {type: String, required: true, lowercase: true},
+  username: {type: String, required: true, unique: true, lowercase: true},
+  password: {type: String, required: true},
+  activated: {type: Boolean, default: false},
+  email: {type: String, required: true, unique: true, lowercase: true},
+  created_at: {type: Date, default: Date.now()},
+  access_level: {
+    admin: {type: Boolean, default: true},
+  },
 });
 
 AdminSchema.pre("save", function (next) {
-	if (!this.isModified("password")) {
-		return next();
-	}
+  if (!this.isModified("password")) {
+    return next();
+  }
 
-	bcrypt.genSalt(10, (err, salt) => {
-		if (err) {
-			return next(err);
-		}
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
 
-		bcrypt.hash(this.password, salt, (err, hash) => {
-			if (err) {
-				return next(err);
-			}
-
-			this.password = hash;
-			next();
-		});
-	});
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      this.password = hash;
+      return next();
+    });
+  });
 });
 
 AdminSchema.methods.comparePass = function (candidate, callback) {
-	bcrypt.compare(candidate, this.password, (err, isMatch) => {
-		if (err) return callback(err);
-		callback(undefined, isMatch);
-	});
+  bcrypt.compare(candidate, this.password, (err, isMatch) => {
+    if (err) return callback(err);
+    callback(undefined, isMatch);
+  });
 };
 
 AdminSchema.statics.byId = function (id, callback) {
-	return this.findOne({_id: id}, callback);
+  return this.findOne({_id: id}, callback);
 };
 
 AdminSchema.statics.byUsername = function (username, callback) {
-	return this.findOne({username: username}, callback);
+  return this.findOne({username: username}, callback);
 };
 
 AdminSchema.statics.byEmail = function (email, callback) {
-	return this.findOne({email: email}, callback);
+  return this.findOne({email: email}, callback);
 };
 
 autoIncrement.initialize(connection);
